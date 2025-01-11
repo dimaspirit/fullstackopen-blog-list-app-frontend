9828acbba23514205adb49c577268e023c260a4d
Dmytro Lobanov <dspwww@gmail.com>
2025-01-11 15:43:49 +0000

    5.1 task DONE

---
+++ src/App.jsx
@@ -0,0 +1,72 @@
+import { useState, useEffect } from 'react'
+import Blog from './components/Blog'
+import blogService from './services/blogs'
+import { login } from './services/login';
+
+const App = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const [user, setUser] = useState(null);
+  const [loginError, setLoginError] = useState(null);
+  const [blogs, setBlogs] = useState([])
+
+  const handleLogin =  async (e) => {
+    e.preventDefault();
+
+    try {
+      const user = await login({username, password});
+      setUser(user);
+
+      setUsername('');
+      setPassword('');
+    } catch (error) {
+      setLoginError(error.response.data.error);
+      setTimeout(() => setLoginError(null), 5000);
+    }
+  }
+
+  useEffect(() => {
+    if(user) {
+      blogService.getAll().then(blogs => setBlogs( blogs ))
+    }
+  }, [user]);
+
+  if(user) {
+    return (
+      <div>
+        <h2>blogs</h2>
+        {blogs.map(blog =>
+          <Blog key={blog.id} blog={blog} />
+        )}
+      </div>
+    )
+  }
+
+  if(!user) {
+    return (
+      <>
+        <h2>Login</h2>
+        <form onSubmit={handleLogin}>
+          {loginError && <p style={{color: 'red'}}>{loginError}</p>}
+
+          <div>
+            <label htmlFor="username">Username</label>
+            <input id="username" type="text" value={username} onChange={e => setUsername(e.target.value)} />
+          </div>
+
+          <div>
+            <label htmlFor="password">Password</label>
+            <input id="password" type="password" value={password} onChange={e => setPassword(e.target.value)} />
+          </div>
+
+          <div>
+            <button type="submit">Login</button>
+          </div>
+        </form>
+      </>
+    )
+  }
+}
+
+export default App
