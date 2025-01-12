43b0a76468d2a24f912a5e7538bdeeb094461a36
Dmytro Lobanov <dspwww@gmail.com>
2025-01-12 22:46:44 +0000

    5.7 task DONE

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,7 +1,32 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} by <b>{blog.author}</b>
-  </div>
-)
+import { useState } from "react"
 
-export default Blog
+const Blog = ({ blog }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const [isFullDescriptionShown, setIsFullDescriptionShown] = useState(false);
+  const buttonLabel = isFullDescriptionShown ? 'hide' : 'show';
+
+  return (
+    <div style={blogStyle}>
+      <h3>
+        {blog.title}
+        <button onClick={() => setIsFullDescriptionShown(!isFullDescriptionShown)}>{buttonLabel}</button>
+      </h3>
+
+      <div style={{display: isFullDescriptionShown ? 'block': 'none'}}>
+        <p>{blog.url}</p>
+        <p>likes: {blog.likes}</p>
+        <p>{blog.author}</p>
+      </div> 
+    </div>
+
+  )
+};
+
+export default Blog;
